0cb5d9c1b87b15261efb45409911c774f4ea4244
Joao <joaomgcroque@gmail.com>
2018-10-09 16:05:15 +0100

    Aula 01

---
+++ MyAula01/MyFortnite/FNPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFortnite
+{
+    class FNPlayer
+    {
+        private int ammo;
+        private float health;
+
+        public FNPlayer()
+        {
+            ammo = 0;
+            health = 100;
+        }
+
+        public FNPlayer (int ammo, float health) {
+            this.ammo = ammo;
+            SetHealth(health);
+
+        }
+
+        public float GetHealth()
+        {
+            return health;
+        }
+        public void SetHealth(float health)
+        {
+            if(health <= 100 && health > 0)
+            {
+                this.health = health;
+            }
+        }
+
+
+        public float Attack(FNPlayer enemy)
+        {
+            float damage = 10;
+            enemy.TakeDamage(damage);
+            return damage;
+
+        }
+        public void TakeDamage(float damage) {
+            health -= damage;
+        }
+    }
+
+}
